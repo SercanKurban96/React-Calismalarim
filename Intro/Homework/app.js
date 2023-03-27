d966a0674e40052f6d76d9106d95cd112d5b2f73
Sercan KURBAN <sercankurban1996@gmail.com>
2023-03-27 11:18:33 +0300

    React Calismalarim

--- Intro/Homework/app.js
+++ Intro/Homework/app.js
@@ -1,17 +1,22 @@
-import axios from "axios";
+// Komut İstemi (cmd) uygulamasından npm i axios kütüphanesi indirildi.
+import axios from "axios"; // axios kütüphanesi import edildi.
 async function getData(Number) {
+  // Fonksiyon asenkron olarak tanımlandı. Fonksiyonun ismi getData olarak verildi. Parametre ismi Number olarak tanımlandı.
   try {
+    // Burada hata yakalama (try - catch) metodu kullanıldı. Asenkron fonksiyonlar await olarak tanımlandı.
     const { data: user } = await axios(
       "https://jsonplaceholder.typicode.com/users/" + Number
     );
     const { data: post } = await axios(
       "https://jsonplaceholder.typicode.com/posts/" + Number
     );
-
+    // Burada loglama işlemleri yapıldı.
     console.log("User: ", user);
     console.log("Post: ", post);
   } catch (e) {
+    // Hata varsa buraya yönlendirilecektir.
     console.log(e);
   }
 }
+// Burada getData fonksiyonu export edildi.
 export default getData;
--- Intro/Homework/index.js
+++ Intro/Homework/index.js
@@ -1,2 +1,2 @@
-import getData from "./app.js";
-getData(1);
+import getData from "./app.js"; // Bu kısımda import edilen app.js fonksiyonu buraya çekildi.
+getData(1); // İlk görevde istenen id değeri verilerek cmd satırında gösterildi.
