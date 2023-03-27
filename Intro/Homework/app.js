22be4eb7062d8ceab3e60435771bbd2b7ce2f515
Sercan KURBAN <sercankurban1996@gmail.com>
2023-03-27 10:58:47 +0300

    React Calismalarim

--- Intro/Homework/app.js
+++ Intro/Homework/app.js
@@ -1,13 +1,17 @@
 import axios from "axios";
-async function getData(id) {
-  const { data: user } = await axios(
-    "https://jsonplaceholder.typicode.com/users/" + id
-  );
-  const { data: post } = await axios(
-    "https://jsonplaceholder.typicode.com/posts/" + id
-  );
+async function getData(Number) {
+  try {
+    const { data: user } = await axios(
+      "https://jsonplaceholder.typicode.com/users/" + Number
+    );
+    const { data: post } = await axios(
+      "https://jsonplaceholder.typicode.com/posts/" + Number
+    );
 
-  console.log("User: ", user);
-  console.log("Post: ", post);
+    console.log("User: ", user);
+    console.log("Post: ", post);
+  } catch (e) {
+    console.log(e);
+  }
 }
 export default getData;
