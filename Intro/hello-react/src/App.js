d4d0122da4b3a9db02b132424a70ab59c930d4c6
Sercan KURBAN <sercankurban1996@gmail.com>
2023-03-29 15:47:49 +0300

    User.js eklendi.

--- Intro/hello-react/src/App.js
+++ Intro/hello-react/src/App.js
@@ -1,21 +1,48 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import Header from "./components/Header";
+// import Header from "./components/Header";
+import User from "./components/User";
 
+// const name = "Sercan";
+// const surname = "Kurban";
+// const isLoggedIn = true;
+
+// const friends = ["Elif", "Ahmet", "Yusuf", "Yeliz", "Leyla"];
+const friends = [
+  { id: 1, name: "Elif" },
+  { id: 2, name: "Ahmet" },
+  { id: 3, name: "Yusuf" },
+  { id: 4, name: "Yeliz" },
+  { id: 5, name: "Leyla" },
+];
 function App() {
   return (
     <>
       {/* <h1>Hello React</h1> */}
-      <Header />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
+      {/* <Header /> */}
+      {/* <h1>{isLoggedIn && `Benim adım ${name}, soyadım ${surname}`}</h1>
+      {!isLoggedIn && "Giriş yapmadınız."} */}
+      {/* Kısa yolu */}
+      {/* <h1>
+        {isLoggedIn
+          ? `Benim adım ${name}, soyadım ${surname}`
+          : "Giriş yapmadınız."}
+      </h1> */}
+
+      {/* <User name="Sercan" surname="Kurban" age={27} isLoggedIn={true} /> */}
+
+      {/* Döngülerde "key" prop'u */}
+      <User
+        name="Sercan"
+        surname="Kurban"
+        isLoggedIn={true}
+        age={27}
+        friends={friends}
+        address={{
+          title: "Ümraniye/İstanbul",
+          zip: 34767,
+        }}
+      />
     </>
   );
 }
---
+++ Intro/hello-react/src/components/User.js
@@ -0,0 +1,48 @@
+// function User({ name, surname, age, isLoggedIn, friends }) {
+//   return (
+//     <>
+//       <h1>
+//         {isLoggedIn ? `${name} ${surname} (${age})` : "Giriş yapmadınız."}
+//       </h1>
+//       {friends.map((friend) => {
+//         return <div key={friend.id}>{friend.name}</div>;
+//       })}
+//     </>
+//   );
+// }
+
+import PropTypes from "prop-types";
+function User({ name, surname, age, isLoggedIn, friends, address }) {
+  if (!isLoggedIn) {
+    return <div>Giriş yapmadınız.</div>;
+  }
+
+  return (
+    <>
+      <h1>{`${name} ${surname} (${age})`}</h1>
+      {address.title} {address.zip} <br />
+      <br />
+      {friends &&
+        friends.map((friend) => <div key={friend.id}>{friend.name}</div>)}
+    </>
+  );
+}
+
+User.propTypes = {
+  name: PropTypes.string.isRequired,
+  surname: PropTypes.string.isRequired,
+  isLoggedIn: PropTypes.bool.isRequired,
+  age: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  friends: PropTypes.array,
+  address: PropTypes.shape({
+    title: PropTypes.string,
+    zip: PropTypes.number,
+  }),
+};
+
+User.defaultProps = {
+  name: "İsimsiz",
+  isLoggedIn: false,
+};
+
+export default User;
