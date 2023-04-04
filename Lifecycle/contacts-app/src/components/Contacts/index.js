6256101896030e25e09f188ff314a62e378b14af
Sercan KURBAN <sercankurban1996@gmail.com>
2023-04-04 16:34:45 +0300

    Contacts added.

--- Lifecycle/contacts-app/src/components/Contacts/index.js
+++ Lifecycle/contacts-app/src/components/Contacts/index.js
@@ -1,12 +1,35 @@
-import React from "react";
+import { useState, useEffect } from "react";
+
+import "./styles.css";
+
 import List from "./List";
 import Form from "./Form";
 
 function Contacts() {
+  const [contacts, setContacts] = useState([
+    {
+      fullname: "Sercan",
+      phone_number: 123123,
+    },
+    {
+      fullname: "Nilgün",
+      phone_number: 123456,
+    },
+    {
+      fullname: "Tomris",
+      phone_number: 654321,
+    },
+  ]);
+
+  useEffect(() => {
+    console.log(contacts);
+  }, [contacts]);
+
   return (
-    <div>
-      <List />
-      <Form />
+    <div id="container">
+      <h1>Contacts</h1>
+      <List contacts={contacts} />
+      <Form addContact={setContacts} contacts={contacts} />
     </div>
   );
 }
